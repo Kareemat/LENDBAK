04d19ffb8f4d867ef2e293b85bca5c2807f221ed
Kareemat <45785816+Kareemat@users.noreply.github.com>
2020-11-25 15:05:32 +0000

    Update COMINGSOON.js

--- COMINGSOON.js
+++ COMINGSOON.js
@@ -9,32 +9,28 @@ $(document).ready(function(){
     });
 });
 
-function submit(){
-  document.getElementById("container").children = "none";
+
+$(document).on('submit', '#myForm', function(e){
   let email= document.getElementById("email")
   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   if (!filter.test(email.value)){ 
     alert('Please provide a valid email address');
   }
   else{
-    document.getElementById('mid').innerHTML = '';
-    document.getElementById("signup").style.display = "none";
-    var message = document.createElement("h1");                
-    message.innerHTML = "We'll keep you informed!";               
-    document.getElementById('mid').appendChild(message);
-  }
-  
-function formSubmit(e) {
-    e.preventDefault(); // prevent actual form submit
-    var form = $("#myForm");
-    var url = form.attr('action'); //get submit url [replace url here if desired]
+    e.preventDefault();
     $.ajax({
-         type: "POST",
-         url: url,
-         data: form.serialize(), // serializes form input
-         success: function(data){
-             console.log(data);
-         }
+      type: 'POST',
+      url: 'https://lendbak-api.workcube.com.ng/api/v1/newsletter/subscribe',
+      data:{
+        email : $('#email').val(),
+      },
+      success:function(){
+        document.getElementById('mid').innerHTML = '';
+        document.getElementById("signup").style.display = "none";
+        var message = document.createElement("h1");                
+        message.innerHTML = "We'll keep you informed!";               
+        document.getElementById('mid').appendChild(message);
+      },
     });
-}
-}
+  }
+});
